a76fd3ff33840a2a06a3f888b21c05b3e857bd06
Laura <lg3295@gmail.com>
2016-03-14 08:57:07 -1000

    Working on deposit cash function

--- doOver.js
+++ doOver.js
@@ -42,18 +42,40 @@ function cashRegister(){
 		secondNum = 0;
 	}
 
-// key onclick registers to output until operator !!vaidate if input === null, input = key, otherwise += key
-// number output is stored in var firstNum and in total !!validate
-// operator is stored in var operator  !!validate
-// total is displayed as output
-// key onclick registers to output until equal button clicked
-// number output is stored in var secondNum !!validate
-// var operator is evaluated to selected correct operation
-// firstNum and secondNum are passed as arguments
-// result stored as total
-// total is display as output
+	function selectEval(operator) {
+			
+		if (operator === '+') {
+			return total = calc.add(firstNum, secondNum);
+		} else if (operator === '-') {
+			return total = calc.subtract(firstNum, secondNum);
+		} else if (operator === '*') {
+			return total = calc.multiply(firstNum, secondNum);
+		} else if (operator === '/') {
+			return total = calc.divide(firstNum, secondNum);
+		}
+	}
 
-//clear sets display value to null
+	function showTotal(total) {
+		return output = total;
+	}
+
+	function depositCash(firstNum) {
+		
+	}
+
+
+	// key onclick registers to output until operator !!vaidate if input === null, input = key, otherwise += key
+	// number output is stored in var firstNum and in total !!validate
+	// operator is stored in var operator  !!validate
+	// total is displayed as output
+		// key onclick registers to output until equal button clicked
+	// number output is stored in var secondNum !!validate
+	// var operator is evaluated to selected correct operation
+	// firstNum and secondNum are passed as arguments
+	// result stored as total
+	// total is display as output
+
+	//clear sets display value to null
 // depositCash adds input value to memory !!validate not negative
 // withdrawCash subtracts input value from memory !!validate not negative, not more than memory
 // getBalance displays memory
